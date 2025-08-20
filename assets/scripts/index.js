61110f4a2876b665535f4da223be3b4e231fc8ea
Alexander Graus <alegraus31@gmail.com>
2025-08-20 04:24:02 -0300

    refactor: sacado scripts de index.html

---
+++ assets/scripts/index.js
@@ -0,0 +1,22 @@
+function salirJuego() {
+    document.querySelector(".game-container").style.display = "flex";
+    document.getElementById("twine-container").style.display = "none";
+}
+
+async function iniciarJuego() {
+    document.querySelector(".game-container").style.display = "none";
+    document.getElementById("twine-container").style.display = "block";
+}
+
+async function pantallaCompleta() {
+    try {
+        if (document.documentElement.requestFullscreen) {
+            await document.documentElement.requestFullscreen();
+        }
+        if (screen.orientation && screen.orientation.lock) {
+            await screen.orientation.lock("landscape");
+        }
+    } catch (err) {
+        console.log("Error al pedir fullscreen:", err);
+    }
+}
